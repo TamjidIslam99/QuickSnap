c5b054ed680b9914c6ea44e9659cb5c9685a5e42
Tamjid <islamtamjid276@gmail.com>
2024-09-11 20:04:39 +0600

    branch1

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -22,6 +22,31 @@ namespace CardGames
 		/// Respond to the user input -- with requests affecting myGame
 		/// </summary>
 		/// <param name="myGame">The game object to update in response to events.</param>
+				private static void DrawGame(Snap myGame)
+		{
+			SwinGame.ClearScreen(Color.White);
+
+			// Draw the top card
+			Card top = myGame.TopCard;
+			if (top != null)
+			{
+				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
+				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, 0, 30);
+				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, 0, 40);
+				SwinGame.DrawCell (SwinGame.BitmapNamed("Cards"), top.CardIndex, 533, 153);
+			}
+			else
+			{
+				SwinGame.DrawText ("No card played yet...", Color.RoyalBlue, 0, 20);
+			}
+
+			// Draw the back of the cards... to represent the deck
+			SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"),52, 155, 153);
+
+			//Draw onto the screen
+			SwinGame.RefreshScreen(60);
+		}
+
 		private static void HandleUserInput(Snap myGame)
 		{
 			//Fetch the next batch of UI interaction
